cc8c0bf709e3e81535e02fcebf745cb7fe82b6cb
Eugene Bobrutskov <sergey.gryaznov.95@mail.ru>
2016-09-16 07:35:23 +0400

    added A+B

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+   int a=0, b=0;
+   std::cout << "enter A and B" << "\n";
+   std::cin >> a >> b;
+   std::cout << a << " " << b << "\n";
+   std::cout << a+b;
+   return 0;
 }
